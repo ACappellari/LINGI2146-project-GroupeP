950132cc81e44fa8f6bfdf258acabca09574571f
noemiebidoul <noemie.bidoul@student.uclouvain.be>
2019-05-20 17:35:25 +0200

    Basic structures and connections

--- Rime_network/sensor_node.c
+++ Rime_network/sensor_node.c
@@ -10,6 +10,37 @@
 #include "dev/light-ziglet.h"
 
 /* STRUCTURES */
+struct sensor_node;
+typedef struct sensor_node{
+	linkaddr_t addr;    
+	uint8_t dist_root;    // Number of hops from root
+} node;
+
+struct sensor_data;
+typedef struct sensor_data{
+	uint16_t val;
+	char* type;         // Temperature or accelerometer data
+} data;
+
+struct child_node;
+typedef struct child_node{
+	rimeaddr_t addr;
+} child;
+
+/* GLOBAL VARIABLES */
+node parent;
+node this;
+child children[MAX_CHILDREN];
+static uint8_t children_numb = 0;
+static uint8_t option = 0;
+
+/* CONNECTIONS */
+/* ----------- */
+
+// Single-hop reliable connections
+static struct runicast_conn routing_conn;
+static struct runicast_conn sensor_conn;
+static struct runicast_conn options_conn;
 
 /* HELPER FUNCTIONS */
 
@@ -17,11 +48,13 @@
 
 /* UNICAST OPTIONS MESSAGES */
 
-/* UNICAST  DATA MESSAGES */
+/* UNICAST DATA MESSAGES */
 
 /* BROADCAST ROUTING MESSAGES */
 
 /* CALLBACKS SETTINGS */
+
+
 
 /*---------------------------------------------------------------------------*/
 PROCESS(sensor_node_process, "Sensor node implementation");
